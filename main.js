af47400437d90a59afffe1f91614dcff54791a44
Alireza Nj <a.nejati1379@gmail.com>
2021-07-29 15:16:31 +0430

    questionA has been changed

--- main.js
+++ main.js
@@ -1,38 +1,67 @@
-document.querySelector("#start").onclick = start();
+document.querySelector("#start").onclick = start;
 
 function start(){
     showQuestion();
 }
 
 function showQuestion(){
-    let zaribs = createZaribs(3);
+    let zaribs = createZaribs(4);
     document.querySelector("#question").innerHTML = createQA(zaribs);
 }
 
 function createQA(zaribs) {
-    let str1="" , str2="", operand = "";
-    if(zaribs[0]===1){
-        str1="x";
-    } else if(zaribs[0]===0){
-        return null;
+    let str2="", str1="", str0="" , operand1 = "", operand2 = "";
+    str2 = checkPowerTwoZarib(zaribs[0]);
+    if(str2===""){
+        str1 = checkPowerOneZarib(zaribs[1]);
     } else {
-        str1 = zaribs[0] + "x";
+        if(zaribs[1]>0){
+            operand1 = " + ";
+            str1 = checkPowerOneZarib(zaribs[1]);
+        } else if(zaribs[1]<0){
+            operand1 = " - ";
+            str1 = checkPowerOneZarib(zaribs[1]*(-1));
+        }
     }
-    if(zaribs[1]<0){
-        operand = " - ";
-        zaribs[1]*=-1;
-        str2 = zaribs[1];
-    } else if(zaribs[1]>0){
-        operand = " + ";
-        str2 = zaribs[1];
+    if(str1==="" && str2===""){
+        return null;
     }
-    let result = str1+operand+str2+" = "+zaribs[2];
-    return result;
+    if(zaribs[2]<0){
+        operand2 = " - ";
+        str0 = zaribs[2]*(-1);
+    } else if(zaribs[2]>0){
+        operand2 = " + ";
+        str0 = zaribs[2];
+    }
+    return str2 + operand1 + str1 + operand2 + str0 + " = " + zaribs[3];
 }
 
-function createQB() {
-    let zaribs = createZaribs(6);
-    return zaribs[0] + "x + " + zaribs[2];
+function createQB(zaribs) {
+
+}
+
+function checkPowerTwoZarib(zarib){
+    if(zarib===0){
+        return "";
+    } else if(zarib===1){
+        return "x^2"
+    } else if(zarib===-1){
+        return "-x^2"
+    } else{
+        return zarib+"x^2"
+    }
+}
+
+function checkPowerOneZarib(zarib){
+    if(zarib===0){
+        return "";
+    } else if(zarib===1){
+        return "x"
+    } else if(zarib===-1){
+        return "-x"
+    } else {
+        return zarib+"x"
+    }
 }
 
 function createRandomNumber(){
