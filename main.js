1d80b8521add20ca503fec703241b4fc817820d0
Alireza Nj <a.nejati1379@gmail.com>
2021-08-06 16:46:03 +0430

    complete js code - TODO : design and improve user interface

--- main.js
+++ main.js
@@ -1,81 +1,123 @@
 document.querySelector("#start").onclick = start;
+let correctAnswers = 0, wrongAnswers = 0, emptyAnswers = 0;
 
 function start(){
+    countdownTimer();
     showQuestion();
     const startButton = document.querySelector("#start");
     startButton.disabled = true;
     const jumpButton = document.querySelector("#jump");
     jumpButton.disabled = false;
-    jumpButton.addEventListener('click' , showQuestion);
+    jumpButton.addEventListener('click' , showQuestionWithJump);
 }
 
-function checkValue(x , y) {
-    if(x===y){
-        alert(true);
+function showQuestionWithJump(){
+    emptyAnswers++;
+    showQuestion();
+}
+
+function checkValue(correct , inputAnswer) {
+    if(correct===inputAnswer){
+        correctAnswers++;
+    } else{
+        wrongAnswers++;
     }
-    alert(false);
+}
+
+function showResult(){
+    document.querySelector("#corrects").innerHTML= correctAnswers.toString();
+    document.querySelector("#wrongs").innerHTML= wrongAnswers.toString();
+    document.querySelector("#empties").innerHTML= emptyAnswers.toString();
+}
+
+function getInputAnswer(value){
+    checkValue(localStorage.getItem("correct"),value);
+    showQuestion();
+}
+
+function showQA(){
+    let zaribs = createZaribs(3);
+    let q = createQA(zaribs);
+    if(q!=null){
+        document.querySelector("#question1").innerHTML = q;
+    } else {
+        showQuestion();
+    }
+    let correct = showOptions(optionsForQAAndQB(answerQA(zaribs))); // showOptions function also return the correct option
+    localStorage.setItem("correct" , correct.toString());
+}
+
+function showQB(){
+    let zaribs = createZaribs(6);
+    document.querySelector("#question1").innerHTML =
+        "<div class=\"flex-container\">\n" +
+        "        <div class=\"flex-item\" id=\"variables\">\n" +
+        "            <div id=\"numerator1\"></div>\n" +
+        "            <hr>\n" +
+        "            <div id=\"denominator1\"></div>\n" +
+        "        </div>\n" +
+        "        <div class=\"flex-item\" id=\"equal\">=</div>\n" +
+        "        <div class=\"flex-item\" id=\"numbers\">\n" +
+        "            <div id=\"numerator2\"></div>\n" +
+        "            <hr>\n" +
+        "            <div id=\"denominator2\"></div>\n" +
+        "        </div>\n" +
+        "    </div>"
+    let sorat = createQB(zaribs.slice(0,2));
+    let makhraj = createQB(zaribs.slice(2,4));
+    let javab = createQBJ(zaribs.slice(4,6));
+    if(javab ===null){
+        showQuestion();
+    } else{
+        document.querySelector("#numerator1").innerHTML = sorat;
+        document.querySelector("#denominator1").innerHTML = makhraj;
+        if(javab===0){
+            document.querySelector("#numerator2").innerHTML = "0";
+        } else {
+            document.querySelector("#numerator2").innerHTML = javab[0];
+            document.querySelector("#denominator2").innerHTML = javab[1];
+        }
+    }
+    let correct = showOptions(optionsForQAAndQB(answerQB(zaribs))); // showOptions function also return the correct option
+    localStorage.setItem("correct" , correct.toString());
+}
+
+function showQC(){
+    let zaribs = createZaribsForC();
+    let q = createQC(zaribs);
+    if(q!=null) {
+        document.querySelector("#question1").innerHTML = "&radic;<span id=\"sqrtQ\"></span>"
+        document.querySelector("#sqrtQ").innerHTML = q;
+    } else {
+        showQuestion();
+    }
+    let correct = showOptions(optionsForQC(answerQC(zaribs))); // showOptions function also return the correct option
+    localStorage.setItem("correct" , correct.toString());
+}
+
+function showQE(){
+    let zaribs = createZaribs(2);
+    let q = createQE(zaribs);
+    if(q!=null) {
+        document.querySelector("#question1").innerHTML = q;
+    } else {
+        showQuestion();
+    }
+    let correct = showOptions(optionsForQE(answerQE(zaribs))); // showOptions function also return the correct option
+    localStorage.setItem("correct" , correct.toString());
 }
 
 function showQuestion(){
-    let x = Math.random();
-    let zaribs , q , answer;
+    showResult();
+    let x = Math.random(); //todo array for functions
     if (x<0.25){
-        zaribs = createZaribs(3);
-        q = createQA(zaribs);
-        if(q!=null){
-            document.querySelector("#question1").innerHTML = q;
-        } else {
-            showQuestion();
-        }
-        let correct = showOptions(optionsForQA(answerQA(zaribs)));
-        
+        showQA();
     } else if(x<0.5){
-        zaribs = createZaribs(2);
-        q = createQE(zaribs);
-        if(q!=null) {
-            document.querySelector("#question1").innerHTML = q;
-        } else {
-            showQuestion();
-        }
+        showQE();
     } else if(x<0.75){
-        zaribs = createZaribsForC();
-        q = createQC(zaribs);
-        if(q!=null) {
-        document.querySelector("#question1").innerHTML = "sqrt("+q+")";
-        } else {
-            showQuestion();
-        }
+        showQC();
     } else{
-        zaribs = createZaribs(6);
-        document.querySelector("#question1").innerHTML =
-        "<div id=\"sorat\" class=\"item1\">\n" +
-        "        <span id=\"sorat1\">\n" +
-        "        </span>\n" +
-        "            <span id=\"equal\">=</span>\n" +
-        "            <span id=\"sorat2\">\n" +
-        "        </span>\n" +
-        "        </div>\n" +
-        "        <div id=\"makhraj\">\n" +
-        "        <span id=\"makhraj1\">\n" +
-        "        </span>\n" +
-        "            <span id=\"makhraj2\">\n" +
-        "        </span>\n" +
-        "        </div>"
-        let sorat = createQB(zaribs.slice(0,2));
-        let makhraj = createQB(zaribs.slice(2,4));
-        let javab = createQBJ(zaribs.slice(4,6));
-        if(javab ===null){
-            showQuestion();
-        } else{
-            document.querySelector("#sorat1").innerHTML = sorat;
-            document.querySelector("#makhraj1").innerHTML = makhraj;
-            if(javab===0){
-                document.querySelector("#sorat2").innerHTML = 0;
-            } else {
-                document.querySelector("#sorat2").innerHTML = javab[0];
-                document.querySelector("#makhraj2").innerHTML = javab[1];
-            }
-        }
+        showQB();
     }
 }
 
@@ -124,22 +166,22 @@ function createQBJ(zaribs){
 }    // e/f
 
 function createQC(zaribs){
-    let str1 = "",str2="",str3="";
-    str3 = zaribs[2].toString();
+    let str1 ,str2,str3;
+    str3 = zaribs[2];
     if(zaribs[0]===1){
         if(zaribs[1]===1){
             return null;
         } else {
-            str2 = (zaribs[1]**2).toString();
-            return str2+" * "+str3;
+            str2 = (zaribs[1]**2);
+            return str2*str3;
         }
     } else {
-        str1=(zaribs[0]**2).toString();
+        str1=(zaribs[0]**2);
         if(zaribs[1]===1){
-            return str1+" * "+str3;
+            return str1 * str3;
         } else {
-            str2 = (zaribs[1]**2).toString();
-            return str1+" * "+str2+" * "+str3;
+            str2 = (zaribs[1]**2);
+            return str1*str2*str3;
         }
     }
 }    // sqrt(a^2b^2c)
@@ -225,22 +267,12 @@ function createZaribs(n){
 }
 
 function createZaribsForC(){
-    let array;
-    let a = Math.round(Math.random()*9+1);
-    let b = Math.round(Math.random()*9+1);
-    let temp = Math.random();
-    let c;
-    if(temp<0.25){
-        c=2;
-    } else if(temp<0.5){
-        c=3;
-    } else if(temp<0.75){
-        c=5;
-    } else {
-        c=7;
-    }
-    array = [a,b,c];
-    return array;
+    let a = Math.round(Math.random()*2+1);
+    let b = Math.round(Math.random()*2+1);
+    let temp = Math.round(Math.random()*3);
+    let arrayC = [2,3,5,7];
+    let c = arrayC[temp];
+    return [a,b,c];
 }
 
 function answerQA(array){
@@ -260,7 +292,7 @@ function answerQA(array){
     return [s,m];
 }
 
-function optionsForQA(answer){
+function optionsForQAAndQB(answer){
     let opt1 , opt2 , opt3 , opt4;
     if(typeof answer === 'number'){
         opt1 = answer;
@@ -283,18 +315,49 @@ function optionsForQA(answer){
 }
 
 function answerQB(array){
-    return (array[3]*array[4]-array[1]*array[5])/(array[0]*array[5]-array[2]*array[4]);
+    let s = array[3]*array[4]-array[1]*array[5];
+    let m = array[0]*array[5]-array[2]*array[4];
+    let g = gcd(s,m);
+    if(m<0){
+        s *= -1;
+        m *= -1;
+    }
+    if(s % m === 0){
+        return s/m;
+    } else if(g !== 1){
+        s /= g;
+        m /= g;
+    }
+    return [s,m];
 }
 
 function answerQC(array){
     let a = array[0]*array[1];
-    return a+"sqrt("+array[2]+")";
+    return [a,array[2]];
+}
+
+function optionsForQC(answer){
+    let opt1 , opt2 , opt3 , opt4;
+    opt1 = answer[0]+ "&radic;<span id=\"sqrtA\">"+answer[1]+"</span>";
+    opt2 = answer[1]+ "&radic;<span id=\"sqrtA\">"+answer[0]+"</span>";
+    opt3 = (answer[0]-1) + "&radic;<span id=\"sqrtA\">"+answer[1]+"</span>";
+    opt4 = answer[0]+ "&radic;<span id=\"sqrtA\">"+(answer[1]+1)+"</span>";
+    return [opt1 , opt2 , opt3 , opt4];
 }
 
 function answerQD(array){}
 
 function answerQE(array){
     return array;
+}
+
+function optionsForQE(answer){
+    let opt1 , opt2 , opt3 , opt4;
+    opt1 = "x<sub>1</sub>="+answer[0]+"<br>"+"x<sub>2</sub>="+answer[1];
+    opt2 = "x<sub>1</sub>="+(answer[0]*-1)+"<br>"+"x<sub>2</sub>="+answer[1];
+    opt3 = "x<sub>1</sub>="+answer[0]+"<br>"+"x<sub>2</sub>="+(answer[1]*-1);
+    opt4 = "x<sub>1</sub>="+(answer[0]*-1)+"<br>"+"x<sub>2</sub>="+(answer[1]*-1);
+    return [opt1 , opt2 , opt3 , opt4];
 }
 
 function gcd(x,y){
@@ -313,7 +376,7 @@ function gcd(x,y){
 function showOptions(array){
     let x = Math.round(Math.random()*3);
     let correct = x+1;
-    let options = document.querySelectorAll(".opt");
+    let options = document.querySelectorAll(".option");
     if(x===0){
         options[0].innerHTML=array[0];
         options[1].innerHTML=array[1];
@@ -336,4 +399,32 @@ function showOptions(array){
         options[2].innerHTML=array[3];
     }
     return correct;
+}
+
+function countdownTimer(){
+    let countDownDate = new Date().getTime()+10100;
+    let x = setInterval(function() {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+        let seconds = Math.floor((distance % (1000 * 60 * 60)) / 1000);
+        document.getElementById("demo").innerHTML =seconds + "s ";
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+            finish();
+        }
+    }, 1000);
+}
+
+function finish(){
+    let score = (correctAnswers*3)-(wrongAnswers);
+    document.querySelector("#start").disabled = false;
+    let options = document.querySelectorAll(".option");
+    for (let option of options) {
+        option.innerHTML="?";
+    }
+    correctAnswers = 0;
+    wrongAnswers = 0;
+    emptyAnswers = 0;
+    alert("your score is : "+score)
 }
